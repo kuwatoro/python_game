d89c43a7bce7ff2bbc19d70d7315687de7cf50f5
sugimuraayumu <70700629+kuwatoro@users.noreply.github.com>
2021-09-09 00:55:01 +0900

    変更点のコミット

---
+++ chapter3/chapter3.py
@@ -0,0 +1,42 @@
+job = "見習い魔術師"
+#print("あなたの職業は" + job + "です")
+
+#print("クラスチェンジした！")
+job = "ブラックマジシャン"
+#print("あなたは" + job)
+
+a = "青眼の白龍" * 3
+#print(a)
+#print("3体融合！")
+b = a
+b = "青眼の究極龍"
+#print(b)
+
+monster = ["セブンスロードマジシャン", "連撃龍ドラギアス", "魔将キメルーラ"]
+#print(monster)
+#print(monster[0])
+#print(monster[2])
+
+
+#print("数値を入力してください")
+#life = int(input())
+#if life <= 0:
+#    print("もうやめて！")
+#if life > 0:
+#    print("まだ俺のバトルフェイズは終了してないぜ！")
+
+#print("清原" + ":" + "お前いくら持ってる？")
+#gold = int(input())
+#if gold == 0:
+#    print("イチロー" + ":" + "テメエに食わせるタンメンはねえ！")
+#else:
+#    print("ダルビッシュ" + ":" + "焼肉行く？")
+
+#for i in range(100, 10, -2):
+#    print(i)
+
+
+i = 0 
+while i <= 5:
+   print(i)
+   i = i + 1
---
+++ chapter3/chapter3_kansuu.py
@@ -0,0 +1,17 @@
+def win(winwin):
+    print("あなたは勝利しました。")
+    print("ボーナス発動！")
+    print("体力が")
+    print(winwin)
+    print("回復した！")
+
+win(100)
+
+def add(gtr, supra):
+    return gtr+supra
+
+iii = add(32, 80)
+print("しかし帰り道に落とし穴に落ちて")
+print(iii)
+print("ダメージ受けた！")
+
